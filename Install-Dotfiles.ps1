77f72b4415bed51f5769c9229ff4866fed535254
chubbyhippo <mkmsacc@outlook.com>
2024-03-23 20:52:53 +0700

    Refresh environment path and add code settings setup
    The recent commits refresh the environment path in the Install-Dotfiles.ps1 script after the integration of the powershell profile. Additionally, setup instructions for code settings have also been added, further enhancing project setup and configuration processes.

--- Install-Dotfiles.ps1
+++ Install-Dotfiles.ps1
@@ -3,6 +3,8 @@ if (-not (Test-Path -Path $PROFILE)) {
     New-Item -ItemType File -Path $PROFILE -Force
 }
 Get-Content -Path .\powershell-profile\Profile.ps1 | Set-Content -Path $PROFILE
+# refresh
+$Env:Path = [System.Environment]::GetEnvironmentVariable("Path","Machine")
 
 # glazeWM
 Copy-Item -Path .\.glaze-wm -Destination $HOME -Recurse -Force
@@ -12,5 +14,11 @@ Copy-Item -Path .\.ideavimrc\.ideavimrc -Destination $HOME -Recurse -Force
 
 # intellij settings
 Set-Location -Path .\Intellij-settings
+& 'C:\Program Files\Git\bin\bash.exe' -c "./install.sh"
+Set-Location -Path ..
+
+# code settings
+Set-Location -Path .\code-settings
+.\Copy-Settings-To-Destination.ps1
 & 'C:\Program Files\Git\bin\bash.exe' -c "./install.sh"
 Set-Location -Path ..
