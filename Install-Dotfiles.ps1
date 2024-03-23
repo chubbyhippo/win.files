7384fe2a7ce587a00f3dc75360660754ee89a399
chubbyhippo <mkmsacc@outlook.com>
2024-03-23 20:00:10 +0700

    Add Intellij-settings to Install-Dotfiles script
    The install script has been updated to include the Intellij-settings repository. This change improves project integration by allowing for the straightforward use of Intellij's settings, contributing to better application synergy and project organization.

--- Install-Dotfiles.ps1
+++ Install-Dotfiles.ps1
@@ -3,3 +3,8 @@ Copy-Item -Path .\.glaze-wm -Destination $HOME -Recurse -Force
 
 # .ideavimrc
 Copy-Item -Path .\.ideavimrc\.ideavimrc -Destination $HOME -Recurse -Force
+
+# intellij settings
+Set-Location -Path .\Intellij-settings
+& 'C:\Program Files\Git\bin\bash.exe' -c "./install.sh"
+Set-Location -Path ..
